ad75ef08fa63e9649a955c65b9889136cf994dce
torobekov005 <azimbektorobekov095@gmail.com>
2024-04-17 09:25:48 +0300

    created

---
+++ app.js
@@ -0,0 +1,38 @@
+
+// const start= document.querySelector("#text");
+
+// const btns = document.querySelectorAll('button');
+
+// console.log(start.innerHTML);
+
+// let count = 0;
+
+// console.log(btns);
+
+// btns[0].onclick =()=>{
+//     count++;
+//     start.innerHTML = count;
+//     start.style.color = 'red'
+// };
+
+// console.log(btns);
+
+// btns[1].onclick =()=>{
+//     count --;
+//     start.innerHTML = count;
+//     start.style.color = 'blue'
+// };
+
+// console.log(btns);
+
+// btns[2].onclick =()=>{
+//     count =0;
+//     start.innerHTML = count ;
+//     start.style.color = 'green'
+// };
+
+
+
+
+
+
